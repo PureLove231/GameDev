96d3f3462894d5eec48be26711aee1da6df2203d
PureLove231 <christianpeitler@icloud.com>
2025-09-02 09:18:19 +0200

    Unity_RPG_Knight

--- RPG_Knight/Assets/Scripts/Movement.cs
+++ RPG_Knight/Assets/Scripts/Movement.cs
@@ -1,10 +1,15 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
 
     [SerializeField] float speed = 5.0f;
+
+    [SerializeField] int faceDirection = 1;
     [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] Animator anim;
 
 
     // Update is called once per frame
@@ -13,7 +18,27 @@ public class Movement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+
         rb.linearVelocity = new Vector2(horizontal, vertical) * speed * Time.deltaTime;
 
+
+        if (horizontal > 0 && transform.localScale.x < 0
+            || horizontal < 0 && transform.localScale.x > 0
+        )
+
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+
+        faceDirection *= -1;
+        transform.localScale = new Vector3(faceDirection, transform.localScale.y, transform.localScale.z);
     }
 }
