ab69d76f7698bfe3eca9feaae4829d3e9ae14783
PureLove231 <christianpeitler@icloud.com>
2025-09-16 19:07:01 +0200

    Made character Move

--- Dungeon_Crawler/Assets/Scripts/Movement.cs
+++ Dungeon_Crawler/Assets/Scripts/Movement.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+    private float horizontalDirection;
+    private float verticalDirection;
+    [SerializeField] float speed = 5.0F;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        horizontalDirection = Input.GetAxis("Horizontal");
+        verticalDirection = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalDirection * speed * Time.deltaTime, verticalDirection * speed * Time.deltaTime);
+
+        rb.MovePosition(rb.position + movement);
+
+
     }
+
+
 }
